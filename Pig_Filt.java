5eb6d946a0bdd5e5d517b3535a07ba2062504655
prasannaksudhakar <prasannaksudhakar@gmail.com>
2017-11-09 09:54:41 +0530

    Add files via upload

---
+++ Pig_Filt.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import org.apache.pig.FilterFunc;
+import org.apache.pig.data.Tuple;
+
+
+public class Pig_Filt extends FilterFunc {
+
+    public Boolean exec(Tuple input) throws IOException {
+		 boolean isTargetAchieved =false;
+        // expect  string
+        String ValueToEval = input.get(0).toString();
+    Float FloatValue = Float.parseFloat(ValueToEval);
+    if ((FloatValue*100)>=80)
+    {
+    	return true;
+    	
+    }
+    else
+    {
+    	return false;
+    }
+        
+    }
+}
